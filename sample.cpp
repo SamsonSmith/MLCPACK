98d8e816738abad4b47a7fca223afb181274d6c6
imaxml <121378484+imaxml@users.noreply.github.com>
2022-12-29 14:25:17 +0530

    Update sample.cpp

--- sample.cpp
+++ sample.cpp
@@ -15,7 +15,7 @@ int main(int argc, char *argv[]) {
 	obj1.loadTrainDb(DB_NAME1);
 	obj1.totalColumn(4);
 	//obj1.outPutType(float);//Not Required obtain from predicted column, default is float.
-	obj1.fit(0,3);
+	obj1.fit(2,3);
 	cout<<"Predicted value for the Entered Col Indexes and Input value : "<<obj1.predict(144.2);
 	cout<<"\n";
 	
@@ -33,8 +33,8 @@ int main(int argc, char *argv[]) {
 	obj3.loadTrainDb(DB_NAME1);
 	obj3.totalColumn(4);
 	//obj3.outPutType(float);//Not Required obtain from predicted column, default is float.
-	obj3.fit(2,3);
-	cout<<"Predicted value for the Entered Col Indexes and Input value : "<<obj3.predict(1262.1);
+	obj3.fit(0,3);
+	cout<<"Predicted value for the Entered Col Indexes and Input value : "<<obj3.predict(385.2);
 	cout<<"\n";
 	return 0;
 }
